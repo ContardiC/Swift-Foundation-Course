be01cdd28d9e515797ddf191985f81fdfb312606
Carlo Contardi <c.contardi@icloud.com>
2025-12-27 17:44:18 +0100

    :add: Lesson002 Playground

---
+++ Lesson002.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: # The operators
+//: Operators are special symbols that allow you to check, modify, or combine values ​​of different types.
+
+/*: There are three types of operators:
+ - Unary operators that act on a single value
+ - Binary operators that act on two values
+ - Ternary operators that act on three values
+ 
+ */
+//: ### Assignement Operator =
+
+let firstName = "Carlo"
+var age = 30
+
+//: ### Arithmetic Operators
+
+let sum = 1 + 2
+let sub = 1 - 2
+let mul = 1 * 2
+let div = 1 / 2
+
+//: ### Remainder Operator
+
+let rem = 4 % 2
+
+//: ### Compound Assignment Operators
+
+age += 1
+age -= 1
+age *= 1
+age /= 1
+
+//: ### Comparison Operators
+
+var a = 2
+var b = 4
+
+a == b
+b != a
+b > a
+b < a
+b >= a
+b <= a
+
+//: ### Logical Operators
+
+var answer1 = true
+var answer2 = false
+
+!answer1
+answer1 && answer2
+answer1 || answer2
+
+//: Learn to code with 🚀 [spacecoding.it](http://spacecoding.it)
